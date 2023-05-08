fd1475656d3852eeb7d0e99b1344a5a30ac53765
sangminnim <sangmins930@gmail.com>
2023-05-08 23:58:00 +0900

    feat: init

---
+++ src/main/java/com/jscode/projectclasssmartsangmin/ProjectClassSmartSangminApplication.java
@@ -0,0 +1,13 @@
+package com.jscode.projectclasssmartsangmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectClassSmartSangminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectClassSmartSangminApplication.class, args);
+	}
+
+}
