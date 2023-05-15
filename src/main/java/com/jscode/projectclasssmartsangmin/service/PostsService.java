1b4eee6eb9687b062eab2c28b0668dbe8b5bfd0f
sangminnim <sangmins930@gmail.com>
2023-05-15 21:30:21 +0900

    feat: impl PostsService

--- src/main/java/com/jscode/projectclasssmartsangmin/service/PostsService.java
+++ src/main/java/com/jscode/projectclasssmartsangmin/service/PostsService.java
@@ -1,10 +1,14 @@
 package com.jscode.projectclasssmartsangmin.service;
 
+import com.jscode.projectclasssmartsangmin.domain.posts.Posts;
 import com.jscode.projectclasssmartsangmin.domain.posts.PostsRepository;
 import com.jscode.projectclasssmartsangmin.dto.PostsSaveRequestDto;
+import com.jscode.projectclasssmartsangmin.exception.PostsNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -14,5 +18,21 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto dto) {
         return postsRepository.save(dto.toEntity()).getId();
+    }
+
+    @Transactional
+    public List<Posts> findAll() {
+        return postsRepository.findAll();
+    }
+
+    @Transactional
+    public Posts findById(Long id) {
+        return postsRepository.findById(id).orElseThrow(PostsNotFoundException::new);
+    }
+
+    @Transactional
+    public void update(Long id, String title,  String content) {
+        Posts posts = postsRepository.findById(id).orElseThrow(PostsNotFoundException::new);
+        posts.update(title, content);
     }
 }
